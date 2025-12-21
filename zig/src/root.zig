eb91db469fa9167e895e3ff37e15a374f81ff3dd
fajaralfa <fajaralfrzi@gmail.com>
2025-12-21 21:58:46 +0700

    make "zig build test" works

--- zig/src/root.zig
+++ zig/src/root.zig
@@ -1,2 +1,8 @@
 pub const assembler = @import("assembler.zig");
 pub const cpu = @import("cpu.zig");
+
+test {
+    // This ensures tests in imported files are discovered
+    _ = @import("assembler.zig");
+    _ = @import("cpu.zig");
+}
