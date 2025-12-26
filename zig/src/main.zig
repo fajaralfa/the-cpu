a9c475d530159dfbe54f1344892e9966cc197432
fajaralfa <fajaralfrzi@gmail.com>
2025-12-26 20:27:23 +0700

    simplify example program

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -26,11 +26,9 @@ pub fn main() !void {
     try machine.addMMIO(allocator, uart_mmio);
     defer machine.deinitMMIO(allocator);
 
-    // program to print ABCDEF
+    // program to print ABCDEF. No cheating, only use general purpose register (2,3,4).
+    // I want to use stack, but this is already make my head twisting.
     const program_words = [_]u16{
-        // target mmio
-        assembler.lui(1, 0xff),
-
         // r2 = 1
         assembler.lui(2, 0),
         assembler.addi(2, 2, 0x1),
@@ -40,37 +38,28 @@ pub fn main() !void {
         assembler.addi(3, 3, 0x6),
 
         // loop:
-        // r2 = r2 << r3
+        // r2 = r2 << r3 (0x40)
         assembler.sll(2, 2, 3),
 
-        // r3 = 1
+        // r2 = r2 + 1 (0x41 / 'A')
+        assembler.addi(2, 2, 0x1),
+
+        // mmio mem[0xFF00] = r2
+        assembler.lui(3, 0xff),
+        assembler.sw(2, 3, 0),
+
+        // loop back to -13
+        assembler.lui(4, 0),
+        assembler.notInstr(4, 4),
         assembler.lui(3, 0),
-        assembler.addi(3, 3, 0x1),
-
-        // r2 = r2 + 1
-        assembler.add(2, 2, 3),
-
-        // mem[0xFF] = r2
-        assembler.sw(2, 1, 0),
-
-        // LOOP BACK
-
-        // for shifting
-        assembler.lui(6, 0),
-        assembler.addi(6, 6, 8),
-
-        // store loop offset in r4 -13 or 0xFFF3 (two's complement)
-        // load upper 0xFF
-        assembler.lui(4, 0xFF),
-
-        // load lower 0xF3
-        assembler.lui(5, 0xF3),
-        assembler.srl(5, 5, 6),
-        assembler.add(4, 4, 5),
+        assembler.addi(3, 3, 0xc),
+        assembler.sub(4, 4, 3),
 
         // store target value in r3 (0x46 for 'F')
-        assembler.lui(3, 0x46),
-        assembler.srl(3, 3, 6),
+        assembler.lui(3, 0),
+        assembler.addi(3, 3, 0x1f),
+        assembler.addi(3, 3, 0x1f),
+        assembler.addi(3, 3, 0x8),
 
         // jump if r2 != r3
         assembler.bne(4, 2, 3),
